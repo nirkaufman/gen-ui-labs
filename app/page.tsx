b590cfe395a5b941f3ee69461979903f7a0fbf88
nirkaufman <nirkaufman@gmail.com>
2025-09-24 11:07:47 +0300

    Feat: update workshop title to include Fundamentals label in Home page header

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20">
       <main className="flex flex-col row-start-2 items-center">
         <h1 className="text-center text-4xl font-bold sm:text-left">
-          <span className="text-orange-400">Generative UI</span> Workshop Labs
+          <span className="text-orange-400">Generative UI</span> Workshop Labs :: <span className='text-gray-400'>Fundamentals</span>
         </h1>
         <small className="text-center text-sm text-stone-400">Nir Kaufman | 2025</small>
         {/*<Chat />*/}
