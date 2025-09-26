5e75822349889847e51d5e278572d5d31b5da497
nirkaufman <nirkaufman@gmail.com>
2025-09-26 12:49:06 +0300

    Feat: add `stepCountIs` utility to enhance AI logic in HomeDecorator actions

--- components/A1-case-study/home-decorator.action.tsx
+++ components/A1-case-study/home-decorator.action.tsx
@@ -5,7 +5,7 @@ import {getMutableAIState, streamUI} from "@ai-sdk/rsc";
 import {openai} from "@ai-sdk/openai";
 import {z} from "zod";
 import ComplementaryColorPicker from "@/components/A1-case-study/ComplementaryColorPicker";
-import {generateId} from "ai";
+import {generateId, stepCountIs} from "ai";
 import SimpleColorPicker from "@/components/A1-case-study/SimpleColorPicker";
 
 export interface ServerMessage {
@@ -26,19 +26,19 @@ export async function HomeDecoratorAction(input: string) {
   const result = await streamUI({
     model: openai('gpt-3.5-turbo'),
     system: `
-    You are a home decorator assistant.
-    The user can ask for color recommendation for his entire home, or a specific room. 
-    When the user ask for a color recommendation, ask him to describe what is the base color of his house or room in words.
-    If the user can't describe it with words, offer to render a color picker. If the user agree, send him a color picker instead. 
-    Once the user select his color, greet him with supportive text and ask him if he would like to get complementary colors recommendation.
-    If the user agree, send him the a complementary color picker
+      You are a home decorator assistant.
+      The user can ask for color recommendation for his entire home, or a specific room. 
+      When the user ask for a color recommendation, ask him to describe what is the base color of his house or room in words.
+      If the user can't describe it with words, offer to render a color picker. If the user agree, send him a color picker instead. 
+      Once the user select his color, greet him with supportive text and ask him if he would like to get complementary colors recommendation.
+      If the user agree, send him the a complementary color picker
     `,
 
     messages: [...history.get(), { role: 'user', content: input }],
 
     text: ({ content, done }) => {
       console.log(content);
-      
+
       if (done) {
         history.done((messages: ServerMessage[]) => [
           ...messages,
